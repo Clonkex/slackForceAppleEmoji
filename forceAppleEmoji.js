ec4eddcf584a9110d5b9c6ed06bd03e56becf244
David <Clonkex@users.noreply.github.com>
2018-02-07 14:53:31 +1100

    Update forceAppleEmoji.js

--- forceAppleEmoji.js
+++ forceAppleEmoji.js
@@ -2,12 +2,22 @@
   'use strict';
 
   var debug = true;
-  var version = "0.3.0";
+  var version = "0.1.0";
 
   // Because sometimes things break and you can't tell if the script
   // is even loading in the Slack app.
   if(debug){
-    alert('slinger version ' + version);
+    alert('slack loader thing version ' + version);
   }
+  
+  console.log('replacing gross google emojis with apple emojis');
+  
+  var slackForceAppleEmoji = $(`
+    <div id="slackForceAppleEmoji" style="display: none;">
+    </div>
+  `);
+  
+  $('#slackForceAppleEmoji').remove();
+  $('body').append(slackForceAppleEmoji);
 
 })();
