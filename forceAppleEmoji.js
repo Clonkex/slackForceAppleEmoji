647fd9e28b96e5b219a9142a8b5078165406ae56
David <Clonkex@users.noreply.github.com>
2018-02-07 14:51:48 +1100

    Update forceAppleEmoji.js

--- forceAppleEmoji.js
+++ forceAppleEmoji.js
@@ -10,49 +10,4 @@
     alert('slinger version ' + version);
   }
 
-  var inApp = window.TSSSB && window.TSSSB.env && window.TSSSB.envdesktop_app_version;
-
-  /////////////////////////////////////////////////////////////////////
-  // Little link for fun.
-  /////////////////////////////////////////////////////////////////////
-  var $slinger = $(`
-    <div id="slinger" style="display: none;">
-      <style>
-        #slinger {
-          display: block !important;
-          position: absolute;
-          z-index: 100000;
-          left: 5px;
-          width: 210px;
-          height: 20px;
-          bottom: 5px;
-          background: #78bbe7;
-          border: 1px solid white;
-          color: white;
-          font-family: Monaco,Menlo,Consolas,"Courier New",monospace;
-          font-size: 10px;
-          text-align: center;
-        }
-
-        #slinger a {
-            pointer: cursor;
-            color: white;
-            line-height: 20px;
-            font-weight: bold;
-            text-decoration: none;
-        }
-
-        #slinger:hover a {
-            text-decoration: underline;
-        }
-
-        #channels_scroller {
-            padding-bottom: 50px;
-        }
-      </style>
-      <a href="https://zachsnow.github.io/slinger/">Pingerz v${version} &raquo;</a>
-    </div>
-  `);
-  $('#slinger').remove();
-  $('body').append($slinger);
 })();
