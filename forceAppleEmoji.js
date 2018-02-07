466cdb9408ab8b4718eeb6eac3b235bff5a6d2ec
David <Clonkex@users.noreply.github.com>
2018-02-07 15:00:13 +1100

    Update forceAppleEmoji.js
    it finally works properly

--- forceAppleEmoji.js
+++ forceAppleEmoji.js
@@ -1,11 +1,9 @@
 (function(){
   'use strict';
 
-  var debug = true;
-  var version = "0.1.0";
+  var debug = false;
+  var version = "0.2.0";
 
-  // Because sometimes things break and you can't tell if the script
-  // is even loading in the Slack app.
   if(debug){
     alert('slack loader thing version ' + version);
   }
