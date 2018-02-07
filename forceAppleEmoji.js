41a521f1ee5bc9218449d938e4ba3e7c9d21b9e0
David <Clonkex@users.noreply.github.com>
2018-02-07 14:54:45 +1100

    Update forceAppleEmoji.js

--- forceAppleEmoji.js
+++ forceAppleEmoji.js
@@ -14,6 +14,11 @@
   
   var slackForceAppleEmoji = $(`
     <div id="slackForceAppleEmoji" style="display: none;">
+      <style>
+        .emoji-outer {
+          background-image: url("https://a.slack-edge.com/c00d19/img/emoji_2017_12_06/sheet_apple_64_indexed_256.png") !important;
+        }
+      </style>
     </div>
   `);
   
